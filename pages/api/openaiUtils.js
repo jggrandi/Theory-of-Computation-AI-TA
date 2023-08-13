6baf8e37299b9e7f7c13015e2c9d733858d8151d
Jeronimo Grandi <jggrandi@gmail.com>
2023-08-13 19:04:27 +0000

    improved the prompt request

--- pages/api/openaiUtils.js
+++ pages/api/openaiUtils.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
@@ -10,18 +10,33 @@ async function createChatCompletion(cachedPrompt, studentMessages) {
         throw new Error("Cached prompt is not yet available.");
     }
 
-    const lastTenMessages = studentMessages.slice(-10);
+    const lastTenMessagesExcludingLast = studentMessages.slice(studentMessages.length - 11, studentMessages.length - 1);
+    const lastMessage = studentMessages.slice(-1)
+    console.log(lastMessage)
 
     const response = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
+            // The orginial prompt
             {
                 "role": "system",
                 "content": cachedPrompt
             },
-            ...lastTenMessages
+            // Last 10 messages for context in follow-up questions
+            ...lastTenMessagesExcludingLast,
+            // Befor the current user question remind the bot of its propose
+            {
+                "role": "user",
+                "content": "Remind me what is your propose?"
+            },
+            {
+                "role": "assistant",
+                "content": "As a teaching assistant in the theory of computation course, my purpose is to provide guidance, hints, and support to help you understand and solve problems related to the course material. I can assist you in understanding concepts, providing hints for problem-solving, and clarifying any doubts you may have. However, I cannot provide direct answers or do the work for you."
+            },
+            // The current user question
+            ...lastMessage,
         ],
-        temperature: 0.15,
+        temperature: 0.5,
         max_tokens: 256,
         top_p: 1,
         frequency_penalty: 0,
